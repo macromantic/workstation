eed1d39c17f90af4282d05bd4cfc3d2943fb3c59
C.Landry <38585729+macromantic@users.noreply.github.com>
2020-06-09 15:33:12 -0400

    Gcp (#5)
    * import license data from gcp
    
    * config sublime user settings

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
 sudo apt install build-essential python python3 ansible puppet -y
-ansible-playbook -i inventory playbooks/site.yml
+ansible-playbook -i inventory playbooks/bootstrap.yml
+$HOME/Downloads/google-cloud-sdk/install.sh
+$HOME/Downloads/google-cloud-sdk/bin/gcloud init
+
 
---
+++ config.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+SUBLIMETEXT_PACKAGES="$HOME/.config/sublime-text-3/Packages"
+SUBLIMETEXT_LOCAL="$HOME/.config/sublime-text-3/Local"
+SUBLIMEMERGE_LOCAL="$HOME/.config/sublime-merge/Local"
+LICENSE_FILE="License.sublime_license"
+
+echo "Configuring Sublime Text License"
+gcloud secrets versions access \
+ "latest" --secret=sublime-text-license >"$SUBLIMETEXT_LOCAL/$LICENSE_FILE"
+echo "Configuring Sublime Merge License"
+gcloud secrets versions access \
+ "latest" --secret=sublime-merge-license >"$SUBLIMEMERGE_LOCAL/$LICENSE_FILE"
+echo "Configuring Sublime Text Settings"
+currentdir=$(pwd)
+cd $SUBLIMETEXT_PACKAGES
+rmdir User
+git clone https://github.com/macromantic/sublime-settings
+mv sublime-settings User
+cd $currentdir
+
