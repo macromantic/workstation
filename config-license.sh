2b254c798bc8442306311f4e21b2450280b9b472
C.Landry <38585729+macromantic@users.noreply.github.com>
2020-06-15 12:06:46 -0400

    separate google cloud sdk config from bootstrap (#10)

--- config-license.sh
+++ config-license.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 SUBLIMETEXT_PACKAGES="$HOME/.config/sublime-text-3/Packages"
 SUBLIMETEXT_LOCAL="$HOME/.config/sublime-text-3/Local"
 SUBLIMEMERGE_LOCAL="$HOME/.config/sublime-merge/Local"
@@ -7,10 +9,10 @@ LICENSE_FILE="License.sublime_license"
 
 echo "Configuring Sublime Text License"
 gcloud secrets versions access \
- "latest" --secret=sublime-text-license >"$SUBLIMETEXT_LOCAL/$LICENSE_FILE"
+    "latest" --secret=sublime-text-license >"$SUBLIMETEXT_LOCAL/$LICENSE_FILE"
 echo "Configuring Sublime Merge License"
 gcloud secrets versions access \
- "latest" --secret=sublime-merge-license >"$SUBLIMEMERGE_LOCAL/$LICENSE_FILE"
+    "latest" --secret=sublime-merge-license >"$SUBLIMEMERGE_LOCAL/$LICENSE_FILE"
 echo "Configuring Sublime Text Settings"
 currentdir=$(pwd)
 cd $SUBLIMETEXT_PACKAGES
@@ -18,4 +20,3 @@ rm -r User
 git clone https://github.com/macromantic/sublime-settings
 mv sublime-settings User
 cd $currentdir
-
