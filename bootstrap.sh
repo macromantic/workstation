0bf526cff2eca34a22bcc3caf5c8a54a3907dcbe
C.Landry <38585729+macromantic@users.noreply.github.com>
2020-06-18 07:46:08 -0400

    need to run gcp playbook before initiating (#11)

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,3 +2,4 @@
 
 sudo apt install build-essential python python3 ansible puppet -y
 ansible-playbook -i inventory playbooks/bootstrap.yml
+
--- google-cloud.sh
+++ google-cloud.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+ansible-playbook -i inventory playbooks/googlecloud.yml
 $HOME/Downloads/google-cloud-sdk/install.sh
 $HOME/Downloads/google-cloud-sdk/bin/gcloud init
